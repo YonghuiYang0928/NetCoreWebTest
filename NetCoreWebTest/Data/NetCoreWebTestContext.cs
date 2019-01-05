59590162373d24eeaae9ef3c3d60be8fb44a3013
YonghuiYang0928 <42133323+YonghuiYang0928@users.noreply.github.com>
2019-01-05 22:25:35 +0800

    init
    1

---
+++ NetCoreWebTest/Data/NetCoreWebTestContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NetCoreWebTest.Models
+{
+    public class NetCoreWebTestContext : DbContext
+    {
+        public NetCoreWebTestContext (DbContextOptions<NetCoreWebTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NetCoreWebTest.Models.Movie> Movie { get; set; }
+    }
+}
